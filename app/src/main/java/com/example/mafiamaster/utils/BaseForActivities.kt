8a9756a9a3b102968426a69dc6e492ce8f2eeaa5
anfo-code <artem29040506000@gmail.com>
2022-07-06 21:01:53 +0200

    add doubleBackToExit

--- app/src/main/java/com/example/mafiamaster/utils/BaseForActivities.kt
+++ app/src/main/java/com/example/mafiamaster/utils/BaseForActivities.kt
@@ -1,10 +1,17 @@
 package com.example.mafiamaster.utils
 
 import android.content.SharedPreferences
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 
 open class BaseForActivities: AppCompatActivity() {
+
+    private var doubleBackToExitPressedOnce = false
+
+
     fun turnOffDarkMode() {
         val appSettingPrefs: SharedPreferences = getSharedPreferences("AppSettingPrefs", 0)
         val sharedPreferencesEditor: SharedPreferences.Editor = appSettingPrefs.edit()
@@ -12,5 +19,22 @@ open class BaseForActivities: AppCompatActivity() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         sharedPreferencesEditor.putBoolean("NightMode", true)
         sharedPreferencesEditor.apply()
+    }
+
+    fun doubleBackToExit() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        } else {
+            this.doubleBackToExitPressedOnce = true
+            Toast.makeText(
+                this,
+                "Please, click back again to exit.",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            Handler(Looper.myLooper()!!)
+                .postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
+        }
     }
 }
